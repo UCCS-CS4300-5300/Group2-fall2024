4c576b2da57f64fd430ef9dddbf669e3d249904b
DavidmKlunk <98335929+DavidmKlunk@users.noreply.github.com>
2024-10-14 09:47:15 -0600

    Final Merge  (#4)
    * Added & Modified logo and styles associated to it
    
    * Deleted extra proxy/3000/ on allowed hosts in settings.file from docker container. It's still working w/ or w/o it
    
    * Added sticky navigation class to the nav section, so game-logo will roll over to other pages
    
    * Calendar steps through utils.py
    
    * Migrated changes
    
    * Added media screen queries for responsive design on the cutom css
    
    * have a rough login and register page and forms  but not sure if they actually work yet
    
    * changed the css in the form template a little
    
    * added calendar view, url, and html
    
    * updated gitignore updated calendar.html and views
    
    * update calendar styles for next/prev month buttons
    
    * Working version of calendar, next/prev, add event
    
    * Updated gitignore for .pyc
    
    ---------
    
    Co-authored-by: hnguye22 <hj2.nguyen0801@gmail.com>
    Co-authored-by: root <98375019+afuina@users.noreply.github.com>
    Co-authored-by: smatthe4 <smatthe4@uccs.edu>
    Co-authored-by: jfuentes-code <jflion@comcast.net>

--- project/home/urls.py
+++ project/home/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from .views import index
+from django.urls import path, re_path
+from . import views
+from .views import *
+from django.contrib.auth import views as auth
 
 urlpatterns = [
     path('', index, name='index'),
+    path('login/',  Login, name ='login'),
+    #path('logout/', auth.LogoutView.as_view(template_name ='logout.html'), name ='logout'),
+    path('register/', Register, name ='register'),
+    path('calendar', CalendarView.as_view(), name = 'calendar' ),
+    re_path(r'^event/new/$', views.event, name='event_new'),
+    re_path(r'^event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
 ]
--- project/home/views.py
+++ project/home/views.py
@@ -1,6 +1,114 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta, date
+from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.utils.safestring import mark_safe
+import calendar
+
+from .models import *
+from .utils import Calendar
+from .forms import EventForm
+from django.contrib.auth.forms import UserCreationForm  
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import UserRegisterForm
+
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('month', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        # Get adjacent months
+        context['prev_month'] = prev_month(d)
+        context['next_month'] = next_month(d)
+
+        return context
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
+
+def prev_month(d):
+    first = d.replace(day=1)
+    prev_month = first - timedelta(days=1)
+    month = 'month=' + str(prev_month.year) + '-' + str(prev_month.month)
+    return month
+
+def next_month(d):
+    days_in_month = calendar.monthrange(d.year, d.month)[1]
+    last = d.replace(day=days_in_month)
+    next_month = last + timedelta(days=1)
+    month = 'month=' + str(next_month.year) + '-' + str(next_month.month)
+    return month
+
+
+def event(request, event_id=None):
+    instance = Event()
+    if event_id:
+        instance = get_object_or_404(Event, pk=event_id)
+    else:
+        instance = Event()
+    
+    form = EventForm(request.POST or None, instance=instance)
+    if request.POST and form.is_valid():
+        form.save()
+        return HttpResponseRedirect(reverse('calendar'))
+    return render(request, 'event.html', {'form': form})
+
+
+
+
+  
+  
+########### register here ##################################### 
+def Register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            ################################################################## 
+            messages.success(request, f'Your account has been created ! You are now able to log in')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'register.html', {'form': form, 'title':'register here'})
+  
+################ login forms################################################### 
+def Login(request):
+    if request.method == 'POST':
+  
+        # AuthenticationForm_can_also_be_used__
+  
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            form = login(request, user)
+            messages.success(request, f' welcome {username} !!')
+            return redirect('index')
+        else:
+            messages.info(request, f'account done not exit plz sign in')
+    form = AuthenticationForm()
+    return render(request, 'login.html', {'form':form, 'title':'log in'})
