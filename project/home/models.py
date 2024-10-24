a41b773ebb942b19cece0bfabed0dc5654abf709
root <98375019+afuina@users.noreply.github.com>
2024-10-24 19:02:31 +0000

    Added priority level option to event model and event_detail.html

--- project/home/models.py
+++ project/home/models.py
@@ -4,11 +4,18 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Event(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'Low'),
+        (2, 'Medium'),
+        (3, 'High'),
+    ]
+
     title = models.CharField(max_length=200)
     description = models.TextField()
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
-
+    priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
+    
     @property
     def get_html_url(self):
         # Return the event_detail link for the event
