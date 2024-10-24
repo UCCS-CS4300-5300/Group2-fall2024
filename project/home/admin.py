3c0c3dbdefeafb7a727154cb6376d14377ba0ba0
root <98375019+afuina@users.noreply.github.com>
2024-10-24 19:28:50 +0000

    Added game model, updated event model to tag game, and added game to event_detail.html

--- project/home/admin.py
+++ project/home/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from home.models import Event
+from home.models import Event, Game
 
 # Register your models here.
 admin.site.register(Event)
+admin.site.register(Game)
--- project/home/models.py
+++ project/home/models.py
@@ -3,6 +3,26 @@ from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
 
 # Create your models here.
+
+# Game model
+class Game(models.Model):
+    PLATFORM_CHOICES = [
+        ('PC', 'PC'),
+        ('PS', 'PlayStation'),
+        ('XBOX', 'Xbox'),
+        ('NS', 'Nintendo Switch'),
+    ]
+
+    name = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100, blank=True, null=True)
+    platform = models.CharField(max_length=50, choices=PLATFORM_CHOICES, blank=True, null=True)
+    developer = models.CharField(max_length=100, blank=True, null=True)
+    release_date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+# Event model 
 class Event(models.Model):
     PRIORITY_CHOICES = [
         (1, 'Low'),
@@ -15,12 +35,14 @@ class Event(models.Model):
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     priority = models.IntegerField(choices=PRIORITY_CHOICES, default=2)
-    
+    game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='events', null=True, blank=True)
+
     @property
     def get_html_url(self):
         # Return the event_detail link for the event
         url = reverse('event_detail', args=(self.id,))
         return f'<a href="{url}"> {self.title} </a>'
+
 
 
 User = get_user_model()
